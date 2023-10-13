3e26ecf8b358c6dbc98a251f4f342c89cead1aab
Grant Ramsay <seapagan@gmail.com>
2023-10-13 19:07:21 +0100

    extract release data and sort PR by date in dict
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- github_changelog_md/changelog/changelog.py
+++ github_changelog_md/changelog/changelog.py
@@ -4,7 +4,8 @@ This will encapsulate the logic for generating the changelog.
 """
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, List, Optional
+import time
+from typing import TYPE_CHECKING, Dict, List, Optional, Tuple
 
 import typer  # pylint: disable=redefined-builtin
 from github import Auth, Github, GithubException
@@ -34,19 +35,22 @@ def git_error(exc: GithubException) -> None:
 class ChangeLog:
     """Define the Changelog class."""
 
+    done_str = "[green]Done[/green]"
+
     def __init__(self, repo_name: str, user_name: Optional[str] = None) -> None:
         """Initialize the class."""
         self.auth = Auth.Token(settings.github_pat)
         self.git = Github(auth=self.auth)
 
-        self.repo_name = repo_name
-        self.user = user_name
+        self.repo_name: str = repo_name
+        self.user: Optional[str] = user_name
 
         self.repo_data: Repository
         self.repo_releases: PaginatedList[GitRelease]
         self.repo_prs: PaginatedList[PullRequest]
-
-        self.filtered_repo_prs: List[PullRequest]
+        self.repo_issues: PaginatedList[Issue]
+        self.release_data: List[Tuple[int, float, str, str]]
+        self.pr_by_date: Dict[float, PullRequest]
         self.filtered_repo_issues: List[Issue]
 
     def run(self) -> None:
@@ -56,82 +60,117 @@ class ChangeLog:
         contains it's own error handling.
         """
         header()
-        # read the repository data
-        self.get_repo_data()
-        # get all the releases
-        self.get_repo_releases()
-        # get all closed PRs
-        self.get_closed_prs()
-        # get closed issues
-        self.get_closed_issues()
+        self.repo_data = self.get_repo_data()
+        self.repo_releases = self.get_repo_releases()
+        self.repo_prs = self.get_closed_prs()
+        self.repo_issues = self.get_closed_issues()
+        # filter out PRs from actual issues (PR's are issues too but
+        # we don't want them in the list).
+        self.filtered_repo_issues = self.filter_issues()
 
-        # filter out non-merged PRs and actual issues (PR's are issues too but
-        # we don't want them in the list)
-        self.filter_prs_and_issues()
+        self.release_data = self.extract_release_data()
+        self.pr_by_date = self.extract_pr_by_date()
 
-    def filter_prs_and_issues(self) -> None:
+        print(self.filtered_repo_issues)
+        print(self.release_data)
+        print(self.pr_by_date)
+
+    def extract_pr_by_date(self) -> Dict[float, PullRequest]:
+        """Extract PRs by date into a dictionary.
+
+        This will enable much faster lookup of PRs by date
+        """
+        print("  [green]->[/green] Extracting PRs by date ... ", end="")
+        pr_by_date = {
+            time.mktime(pr.merged_at.timetuple()): pr
+            for pr in self.repo_prs
+            if pr.merged_at
+        }
+        print(self.done_str)
+        return pr_by_date
+
+    def extract_release_data(self) -> List[Tuple[int, float, str, str]]:
+        """Extract creation dates and more for each Release.
+
+        return a list of tuples
+        """
+        print("\n  [green]->[/green] Extracting Release data ... ", end="")
+        release_data = []
+        for release in self.repo_releases:
+            release_data.append(
+                (
+                    release.id,
+                    time.mktime(release.created_at.timetuple()),
+                    release.title,
+                    release.tag_name,
+                )
+            )
+        print(self.done_str)
+        return release_data
+
+    def filter_issues(self) -> List[Issue]:
         """Filter out non-merged PRs and actual issues."""
-        print("\n  [green]->[/green] Filtering PRs and Issues ... ", end="")
-        self.filtered_repo_prs = [pr for pr in self.repo_prs if pr.merged_at]
-        self.filtered_repo_issues = [
+        print("\n  [green]->[/green] Filtering Issues from PRs... ", end="")
+        filtered_repo_issues = [
             issue for issue in self.repo_issues if not issue.pull_request
         ]
-        print("[green]Done[/green]")
+        print(self.done_str)
 
         print(
-            f"  [green]->[/green] Found [green]{len(self.filtered_repo_prs)}"
-            f"[/green] Merged PRs and [green]{len(self.filtered_repo_issues)}"
+            f"  [green]->[/green] Found [green]"
+            f"{len(filtered_repo_issues)}"
             "[/green] Actual Closed Issues"
         )
+        return filtered_repo_issues
 
-    def get_closed_issues(self) -> None:
+    def get_closed_issues(self) -> PaginatedList[Issue]:  # type: ignore
         """Get info on all the closed issues from GitHub."""
         print("  [green]->[/green] Getting Closed Issues ... ", end="")
         try:
-            self.repo_issues = self.repo_data.get_issues(
+            repo_issues = self.repo_data.get_issues(
                 state="closed", sort="created"
             )
         except GithubException as exc:
             git_error(exc)
         else:
-            print(f"[green]{self.repo_issues.totalCount} Found[/green]")
+            print(f"[green]{repo_issues.totalCount} Found[/green]")
+            return repo_issues
 
-    def get_closed_prs(self) -> None:
+    def get_closed_prs(self) -> PaginatedList[PullRequest]:  # type: ignore
         """Get info on all the closed PRs from GitHub."""
         print("  [green]->[/green] Getting Closed PRs ... ", end="")
         try:
-            self.repo_prs = self.repo_data.get_pulls(
-                state="closed", sort="created"
-            )
+            repo_prs = self.repo_data.get_pulls(state="closed", sort="created")
         except GithubException as exc:
             git_error(exc)
         else:
-            print(f"[green]{self.repo_prs.totalCount} Found[/green]")
+            print(f"[green]{repo_prs.totalCount} Found[/green]")
+            return repo_prs
 
-    def get_repo_releases(self) -> None:
+    def get_repo_releases(self) -> PaginatedList[GitRelease]:  # type: ignore
         """Get info on all the releases from GitHub."""
         print("  [green]->[/green] Getting Releases ... ", end="")
         try:
-            self.repo_releases = self.repo_data.get_releases()
+            repo_releases = self.repo_data.get_releases()
         except GithubException as exc:
             git_error(exc)
         else:
-            print(f"[green]{self.repo_releases.totalCount} Found[/green]")
+            print(f"[green]{repo_releases.totalCount} Found[/green]")
+            return repo_releases
 
-    def get_repo_data(self) -> None:
+    def get_repo_data(self) -> Repository:  # type: ignore
         """Read the repository data from GitHub."""
         print("  [green]->[/green] Getting Repository data ... ", end="")
         try:
             repo_user = self.user if self.user else self.git.get_user().login
 
-            self.repo_data = self.git.get_user(repo_user).get_repo(
-                self.repo_name
-            )
+            repo_data = self.git.get_user(repo_user).get_repo(self.repo_name)
         except GithubException as exc:
             git_error(exc)
         else:
-            print("[green]Done[/green]")
+            print(self.done_str)
             print(
                 "  [green]->[/green] Repository : "
-                f"[bold]{self.repo_data.full_name}[/bold]"
+                f"[bold]{repo_data.full_name}[/bold]"
             )
+            return repo_data
