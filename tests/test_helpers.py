e49971062574a9db8e4dc33647023b85ef33fb33
Grant Ramsay <seapagan@gmail.com>
2023-10-20 11:37:51 +0100

    convert tests to classes and add full typing
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -8,95 +8,104 @@ from github_changelog_md.constants import ExitErrors
 from github_changelog_md.helpers import get_app_version, get_repo_name, header
 
 if TYPE_CHECKING:
+    import pytest
+    from pyfakefs.fake_filesystem import FakeFileSystem
     from pytest_mock import MockerFixture
 
 
-def test_get_repo_name_with_git_config(mocker: MockerFixture) -> None:
-    """Test get_repo_name function with a valid git config file."""
-    mocker.patch(
-        "pathlib.Path.open",
-        mocker.mock_open(
-            read_data=(
-                '[remote "origin"]\n' "url = https://github.com/user/repo.git\n"
-            )
-        ),
-    )
-    assert get_repo_name() == "repo"
+class TestHelpers:
+    """Test class for the 'helpers' module."""
 
+    patch_get_toml = "github_changelog_md.helpers.get_toml_path"
+    test_toml_path = "tests/data/pyproject.toml"
 
-def test_get_repo_name_without_git_config(mocker: MockerFixture) -> None:
-    """Test get_repo_name function without a git config file."""
-    mocker.patch("pathlib.Path.exists", return_value=False)
-    assert get_repo_name() is None
+    def test_get_repo_name_with_git_config(self, mocker: MockerFixture) -> None:
+        """Test get_repo_name function with a valid git config file."""
+        mocker.patch(
+            "pathlib.Path.open",
+            mocker.mock_open(
+                read_data=(
+                    '[remote "origin"]\n'
+                    "url = https://github.com/user/repo.git\n"
+                )
+            ),
+        )
+        assert get_repo_name() == "repo"
 
+    def test_get_repo_name_without_git_config(
+        self, mocker: MockerFixture
+    ) -> None:
+        """Test get_repo_name function without a git config file."""
+        mocker.patch("pathlib.Path.exists", return_value=False)
+        assert get_repo_name() is None
 
-def test_header(capsys):
-    header()
-    captured = capsys.readouterr()
-    assert "GitHub Changelog Generator" in captured.out
+    def test_header(self, capsys: pytest.CaptureFixture[str]) -> None:
+        header()
+        captured = capsys.readouterr()
+        assert "GitHub Changelog Generator" in captured.out
 
+    def test_get_app_version_with_toml_file(
+        self, fs: FakeFileSystem, mocker: MockerFixture
+    ) -> None:
+        """Test get_app_version function with a valid pyproject.toml file."""
 
-def test_get_app_version_with_toml_file(fs, mocker: MockerFixture) -> None:
-    """Test get_app_version function with a valid pyproject.toml file."""
+        fs.create_file(
+            self.test_toml_path,
+            contents=(
+                "[tool.poetry]\n"
+                'name = "github_changelog_md"\n'
+                'version = "0.5.0"\n'
+            ),
+        )
+        mocker.patch(
+            self.patch_get_toml,
+            return_value=Path(self.test_toml_path),
+        )
+        assert get_app_version() == "0.5.0"
 
-    fs.create_file(
-        "/tests/data/pyproject.toml",
-        contents=(
-            "[tool.poetry]\n"
-            'name = "github_changelog_md"\n'
-            'version = "0.5.0"\n'
-        ),
-    )
-    mocker.patch(
-        "github_changelog_md.helpers.get_toml_path",
-        return_value=Path("tests/data/pyproject.toml"),
-    )
-    assert get_app_version() == "0.5.0"
+    def test_get_app_version_bad_toml_file(
+        self, fs: FakeFileSystem, mocker: MockerFixture
+    ) -> None:
+        """Test get_app_version function with a bad pyproject.toml file."""
 
+        fs.create_file(
+            self.test_toml_path,
+            contents=("[tool.poetry]\n" 'name = "github_changelog_md"\n'),
+        )
+        mocker.patch(
+            self.patch_get_toml,
+            return_value=Path(self.test_toml_path),
+        )
+        mocked_exit = mocker.patch("github_changelog_md.helpers.sys.exit")
 
-def test_get_app_version_bad_toml_file(fs, mocker: MockerFixture) -> None:
-    """Test get_app_version function with a bad pyproject.toml file."""
+        assert get_app_version() is None
+        assert mocked_exit.call_args[0][0] == ExitErrors.OS_ERROR
 
-    fs.create_file(
-        "/tests/data/pyproject.toml",
-        contents=("[tool.poetry]\n" 'name = "github_changelog_md"\n'),
-    )
-    mocker.patch(
-        "github_changelog_md.helpers.get_toml_path",
-        return_value=Path("tests/data/pyproject.toml"),
-    )
-    mocked_exit = mocker.patch("github_changelog_md.helpers.sys.exit")
+    def test_get_app_version_from_metadata(self, mocker: MockerFixture) -> None:
+        """Test get_app_version function without a pyproject.toml file."""
 
-    assert get_app_version() is None
-    assert mocked_exit.call_args[0][0] == ExitErrors.OS_ERROR
+        mocker.patch(
+            self.patch_get_toml,
+            return_value=Path(self.test_toml_path),
+        )
+        mocker.patch(
+            "github_changelog_md.helpers.metadata.version",
+            return_value="0.5.0",
+        )
+        assert get_app_version() == "0.5.0"
 
+    def test_get_app_version_not_found(self, mocker: MockerFixture) -> None:
+        """Test get_app_version function without a pyproject.toml file."""
 
-def test_get_app_version_from_metadata(mocker: MockerFixture) -> None:
-    """Test get_app_version function without a pyproject.toml file."""
+        mocker.patch(
+            self.patch_get_toml,
+            return_value=Path(self.test_toml_path),
+        )
+        mocker.patch(
+            "github_changelog_md.helpers.metadata.version",
+            side_effect=metadata.PackageNotFoundError("Error message"),
+        )
+        mocked_exit = mocker.patch("github_changelog_md.helpers.sys.exit")
 
-    mocker.patch(
-        "github_changelog_md.helpers.get_toml_path",
-        return_value=Path("tests/data/pyproject.toml"),
-    )
-    mocker.patch(
-        "github_changelog_md.helpers.metadata.version",
-        return_value="0.5.0",
-    )
-    assert get_app_version() == "0.5.0"
-
-
-def test_get_app_version_not_found(mocker: MockerFixture) -> None:
-    """Test get_app_version function without a pyproject.toml file."""
-
-    mocker.patch(
-        "github_changelog_md.helpers.get_toml_path",
-        return_value=Path("tests/data/pyproject.toml"),
-    )
-    mocker.patch(
-        "github_changelog_md.helpers.metadata.version",
-        side_effect=metadata.PackageNotFoundError("Error message"),
-    )
-    mocked_exit = mocker.patch("github_changelog_md.helpers.sys.exit")
-
-    assert get_app_version() is None
-    assert mocked_exit.call_args[0][0] == ExitErrors.OS_ERROR
+        assert get_app_version() is None
+        assert mocked_exit.call_args[0][0] == ExitErrors.OS_ERROR
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
 from unittest.mock import Mock
 
 import pytest
@@ -5,80 +8,92 @@ from typer.testing import CliRunner
 
 from github_changelog_md.main import app
 
+if TYPE_CHECKING:
+    from pytest_mock import MockerFixture
+    from pytest_mock.plugin import MockType
+
 
 @pytest.fixture()
-def mock_changelog(mocker):
+def mock_changelog(mocker: MockerFixture) -> MockType:
     return mocker.patch("github_changelog_md.main.ChangeLog")
 
 
-def test_main_with_versio():
-    runner = CliRunner()
-    result = runner.invoke(app, ["--version"])
-    assert "Github Changelog Markdown" in result.output
+class TestMain:
+    """Test class for the 'main' module."""
 
+    def test_main_with_versio(self) -> None:
+        runner = CliRunner()
+        result = runner.invoke(app, ["--version"])
+        assert "Github Changelog Markdown" in result.output
 
-def test_main_with_repo(mock_changelog):
-    mock_changelog_instance = Mock()
-    mock_changelog.return_value = mock_changelog_instance
+    def test_main_with_repo(self, mock_changelog: MockType) -> None:
+        mock_changelog_instance = Mock()
+        mock_changelog.return_value = mock_changelog_instance
 
-    runner = CliRunner()
-    runner.invoke(app, ["--repo", "test_repo"])
-    mock_changelog.assert_called_once_with("test_repo", None, None)
-    mock_changelog_instance.run.assert_called_once()
+        runner = CliRunner()
+        runner.invoke(app, ["--repo", "test_repo"])
+        mock_changelog.assert_called_once_with("test_repo", None, None)
+        mock_changelog_instance.run.assert_called_once()
 
+    def test_main_with_repo_and_user(self, mock_changelog: MockType) -> None:
+        mock_changelog_instance = Mock()
+        mock_changelog.return_value = mock_changelog_instance
 
-def test_main_with_repo_and_user(mock_changelog):
-    mock_changelog_instance = Mock()
-    mock_changelog.return_value = mock_changelog_instance
+        runner = CliRunner()
+        runner.invoke(app, ["--repo", "test_repo", "--user", "test_user"])
+        mock_changelog.assert_called_once_with("test_repo", "test_user", None)
+        mock_changelog_instance.run.assert_called_once()
 
-    runner = CliRunner()
-    runner.invoke(app, ["--repo", "test_repo", "--user", "test_user"])
-    mock_changelog.assert_called_once_with("test_repo", "test_user", None)
-    mock_changelog_instance.run.assert_called_once()
+    def test_main_with_repo_and_user_and_next_release(
+        self,
+        mock_changelog: MockType,
+    ) -> None:
+        mock_changelog_instance = Mock()
+        mock_changelog.return_value = mock_changelog_instance
 
+        runner = CliRunner()
+        runner.invoke(
+            app,
+            [
+                "--repo",
+                "test_repo",
+                "--user",
+                "test_user",
+                "--next-release",
+                "v1.0",
+            ],
+        )
+        mock_changelog.assert_called_once_with("test_repo", "test_user", "v1.0")
+        mock_changelog_instance.run.assert_called_once()
 
-def test_main_with_repo_and_user_and_next_release(mock_changelog):
-    mock_changelog_instance = Mock()
-    mock_changelog.return_value = mock_changelog_instance
+    def test_no_repo_specified_get_from_local_repo(
+        self, mocker: MockerFixture, mock_changelog: MockType
+    ) -> None:
+        mock_changelog_instance = Mock()
+        mock_changelog.return_value = mock_changelog_instance
 
-    runner = CliRunner()
-    runner.invoke(
-        app,
-        [
-            "--repo",
-            "test_repo",
-            "--user",
-            "test_user",
-            "--next-release",
-            "v1.0",
-        ],
-    )
-    mock_changelog.assert_called_once_with("test_repo", "test_user", "v1.0")
-    mock_changelog_instance.run.assert_called_once()
+        mocker.patch(
+            "github_changelog_md.main.get_repo_name",
+            return_value="test_local_repo",
+        )
 
+        runner = CliRunner()
+        runner.invoke(app)
+        mock_changelog.assert_called_once_with("test_local_repo", None, None)
+        mock_changelog_instance.run.assert_called_once()
 
-def test_no_repo_specified_get_from_local_repo(mocker, mock_changelog):
-    mock_changelog_instance = Mock()
-    mock_changelog.return_value = mock_changelog_instance
+    def test_no_repo_specified_and_no_local_repo_found(
+        self, mocker: MockerFixture, mock_changelog: MockType
+    ) -> None:
+        mock_changelog_instance = Mock()
+        mock_changelog.return_value = mock_changelog_instance
 
-    mocker.patch(
-        "github_changelog_md.main.get_repo_name", return_value="test_local_repo"
-    )
+        mocker.patch(
+            "github_changelog_md.main.get_repo_name", return_value=None
+        )
 
-    runner = CliRunner()
-    runner.invoke(app)
-    mock_changelog.assert_called_once_with("test_local_repo", None, None)
-    mock_changelog_instance.run.assert_called_once()
-
-
-def test_no_repo_specified_and_no_local_repo_found(mocker, mock_changelog):
-    mock_changelog_instance = Mock()
-    mock_changelog.return_value = mock_changelog_instance
-
-    mocker.patch("github_changelog_md.main.get_repo_name", return_value=None)
-
-    runner = CliRunner()
-    result = runner.invoke(app)
-    assert "Could not find a local repository" in result.output
-    mock_changelog.assert_not_called()
-    mock_changelog_instance.run.assert_not_called()
+        runner = CliRunner()
+        result = runner.invoke(app)
+        assert "Could not find a local repository" in result.output
+        mock_changelog.assert_not_called()
+        mock_changelog_instance.run.assert_not_called()
