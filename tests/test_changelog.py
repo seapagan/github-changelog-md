df162911ca8070bc2e4b016c15f13898be6cc194
Grant Ramsay <seapagan@gmail.com>
2023-10-21 12:54:40 +0100

    start work on testing main ChangeLog class
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

---
+++ tests/test_changelog.py
@@ -0,0 +1,24 @@
+"""Test the ChangeLog class."""
+
+import pytest
+import typer
+from github import GithubException
+
+from github_changelog_md.changelog.changelog import git_error
+from github_changelog_md.constants import ExitErrors
+
+
+class TestChangelog:
+    """Class with all tests for the ChangeLog class."""
+
+    def test_git_error(self) -> None:
+        """Test the git_error method."""
+        git_exception = GithubException(
+            status=404,
+            data={"message": "Not Found"},
+        )
+
+        with pytest.raises(typer.Exit) as exc:
+            git_error(git_exception)
+
+        assert exc.value.args[0] == ExitErrors.GIT_ERROR
